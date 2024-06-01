d19a71d2bfeb3a5e368228a9438ffaa010aa0fce
AhmadSibgatullah <ahmad851133@smk.belajar.id>
2024-06-01 15:19:27 +0700

    Tugas Nabil

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
 use App\Models\Kategori;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +22,13 @@ Route::controller(KategoriController::class)->group(function () {
     Route::get('/kategori-edit/{id}', 'edit')->name('kategori.edit');
     Route::put('/kategori-edit/{id}', 'update')->name('kategori.update');
     Route::delete('kategori/{id}', 'destroy')->name('kategori.delete');
+});
+
+Route::controller(SiswaController::class)->group(function () {
+    Route::get('/siswa', 'index')->name('siswa');
+    Route::get('/siswa-create', 'create')->name('siswa.create');
+    Route::post('/siswa', 'store')->name('siswa.perform');
+    Route::get('/siswa-edit/{id}', 'edit')->name('siswa.edit');
+    Route::put('/siswa-edit/{id}', 'update')->name('siswa.update');
+    Route::delete('siswa/{id}', 'destroy')->name('siswa.delete');
 });
